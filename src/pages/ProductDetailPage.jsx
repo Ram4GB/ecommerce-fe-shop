8693e266afffb66ff66f0ee1298f41bfc2e4fde1
Hoang Tran <36368107+HoangTran0410@users.noreply.github.com>
2020-04-12 14:11:21 +0700

    basic carousel

--- src/pages/ProductDetailPage.jsx
+++ src/pages/ProductDetailPage.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+import React, { useState } from "react";
 import { Grid, useMediaQuery } from "@material-ui/core";
 import Rating from "@material-ui/lab/Rating";
 import FavoriteIcon from "@material-ui/icons/Favorite";
@@ -11,17 +13,49 @@ import p4 from "../commons/assets/img/products/large/product-4.jpg";
 export default function ProductDetailPage() {
   const isMobile = useMediaQuery("(max-width:504px)");
 
+  const listImages = [p1, p2, p3, p4];
+  const [carouselIndex, setCarouselIndex] = useState(0); // index (in listImages) of first image in carousel
+  const [currentPreview, setCurrentPreview] = useState(p1);
+
+  // return an images array base on carouselIndex
+  const getCarouselImages = () => {
+    return [...listImages.slice(carouselIndex), ...listImages.slice(0, carouselIndex)];
+  };
+
+  const pre = () => {
+    let newIndex = carouselIndex - 1;
+    if (newIndex < 0) newIndex = listImages.length - 1;
+    setCarouselIndex(newIndex);
+  };
+  const next = () => {
+    let newIndex = carouselIndex + 1;
+    if (newIndex >= listImages.length) newIndex = 0;
+    setCarouselIndex(newIndex);
+  };
+
   const renderListImage = () => {
     return (
-      <div>
-        {[p1, p2, p3, p4].map(src => (
-          <img key={src} className="img-preview-small" src={src} alt="" />
+      <div className="carousel-container">
+        <button type="button" className="btn-carousel left" onClick={pre}>
+          &lsaquo;
+        </button>
+        {getCarouselImages().map(src => (
+          <img
+            key={src}
+            className="img-preview-small"
+            src={src}
+            alt=""
+            onClick={() => setCurrentPreview(src)}
+          />
         ))}
+        <button type="button" className="btn-carousel right" onClick={next}>
+          &rsaquo;
+        </button>
       </div>
     );
   };
 
-  const listCategories = ["Fashion", "Electronics", "Toys", "Food", "Jewellery"];
+  const listCategories = ["Fashion", "Electronics", "Toys", "Food", "Car"];
 
   return (
     <div className="w-90 product-detail-page">
@@ -29,7 +63,7 @@ export default function ProductDetailPage() {
         {/* product image preview */}
         <Grid item xs={12} sm={12} md={6} lg={6}>
           <div className="image-preview-container" style={isMobile ? {} : { marginRight: "40px" }}>
-            <img className="img-preview-big" src={p1} alt="" />
+            <img className="img-preview-big" src={currentPreview} alt="" />
             {renderListImage()}
           </div>
         </Grid>
