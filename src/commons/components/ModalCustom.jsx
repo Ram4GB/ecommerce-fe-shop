d1d24d701e66625eaa1271cb08e7632f14c418d7
Ram4GB <leminhcuong2988@yahoo.com.vn>
2020-04-30 21:13:49 +0700

    add dialog and modal custom add notfound page

---
+++ src/commons/components/ModalCustom.jsx
@@ -0,0 +1,22 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React from "react";
+import { Modal } from "@material-ui/core";
+import PropTypes from "prop-types";
+
+export default function ModalCustom({ children, onClose, open, ...props }) {
+  return (
+    <Modal open={open} onClose={onClose} {...props}>
+      <div>{children}</div>
+    </Modal>
+  );
+}
+
+ModalCustom.propTypes = {
+  children: PropTypes.element,
+  onClose: PropTypes.func.isRequired,
+  open: PropTypes.bool.isRequired
+};
+
+ModalCustom.defaultProps = {
+  children: <></>
+};
