85397fa3054a56de8542569adb46872828a2fa53
Ram4GB <leminhcuong2988@yahoo.com.vn>
2020-05-14 15:37:30 +0700

    checkout page v2

--- src/modules/ui/components/CheckoutCarViewInfomation.jsx
+++ src/modules/ui/components/CheckoutCarViewInfomation.jsx
@@ -1,11 +1,56 @@
+/* eslint-disable react/no-array-index-key */
 import React from "react";
 import { Grid } from "@material-ui/core";
+import { useDispatch } from "react-redux";
+import { SET_CURRENT_PAGE_CHECKOUT_PAGE } from "../reducers";
+
+const product = {
+  Variations: [{ colors: "00005c,6a097d" }, { colors: "c060a1,ffdcb4" }]
+};
 
 export default function CheckoutCarViewInfomation() {
+  const dispatch = useDispatch();
+
+  const renderVariations = () => {
+    const array = [];
+    const defaultWidth = 50;
+
+    if (product && product.Variations) {
+      product.Variations.forEach((item, index2) => {
+        const colors = item.colors.split(",");
+        const temp = [];
+        colors.forEach((color, index) => {
+          temp.push(
+            <span
+              key={`color-${index}`}
+              style={{
+                backgroundColor: `#${color}`,
+                width: `${defaultWidth / colors.length}px`,
+                height: defaultWidth
+              }}
+              className="color-dot"
+            />
+          );
+        });
+        array.push(
+          <span
+            key={`color-group-${index2}`}
+            style={{ width: defaultWidth, height: defaultWidth }}
+            className="color-dot-group"
+          >
+            {temp}
+          </span>
+        );
+      });
+      return array;
+    }
+    return "";
+  };
+
   return (
     <div className="checkout-cart-view-information">
-      <Grid style={{ height: "100%" }} container>
-        <Grid className="car-img" item lg={8}>
+      <Grid container>
+        <Grid className="car-img" sm={12} xs={12} item md={12} lg={8}>
           <div className="wrap-image">
             <img
               src="https://static-assets.tesla.com/configurator/compositor?&options=$W38B,$PPSW,$DV2W,$MT308,$IN3B2&view=STUD_3QTR&model=m3&size=1441&bkba_opt=1&version=v0027d202005074910&version=v0027d202005074910"
@@ -15,27 +60,28 @@ export default function CheckoutCarViewInfomation() {
           <div className="small-info">
             <div className="info">
               <h3 className="number">250mi</h3>
-              <p>Range</p>
+              <p className="desc">Range</p>
             </div>
             <div className="info">
               <h3 className="number">140mph</h3>
-              <p>Top Speed</p>
+              <p className="desc">Top Speed</p>
             </div>
             <div className="info">
               <h3 className="number">5.3s</h3>
-              <p>0-60 mph</p>
+              <p className="desc">0-60 mph</p>
             </div>
           </div>
         </Grid>
-        <Grid className="variatation" item lg={4}>
-          <h3 className="select-color">Select Color</h3>
-          <div className="group-color">
-            <span className="color" />
-            <span className="color" />
-            <span className="color" />
-            <span className="color" />
-            <span className="color" />
-          </div>
+        <Grid className="variatation" item sm={12} xs={12} md={12} lg={4}>
+          <p className="select-color">Select Color</p>
+          <div>{renderVariations()}</div>
+          <button
+            onClick={() => dispatch(SET_CURRENT_PAGE_CHECKOUT_PAGE("#finance-option"))}
+            className="button-next"
+            type="button"
+          >
+            Next Step
+          </button>
         </Grid>
       </Grid>
     </div>
--- src/pages/CheckoutVersionTwo.jsx
+++ src/pages/CheckoutVersionTwo.jsx
@@ -1,17 +1,28 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React from "react";
 import { useSelector } from "react-redux";
+import { FormContext, useForm } from "react-hook-form";
 import { MODULE_NAME as MODULE_UI } from "../modules/ui/models";
 import CheckoutCarViewInfomation from "../modules/ui/components/CheckoutCarViewInfomation";
+import CheckoutPayment from "../modules/ui/components/CheckoutPayment";
+import CheckoutFinanceOptions from "../modules/ui/components/CheckoutFinanceOption";
 
 export default function CheckoutVersionTwo() {
   const activeNavItem = useSelector(state => state[MODULE_UI].checkoutPage.activeNavItem);
+  const methods = useForm();
 
-  switch (activeNavItem) {
-    case "#car":
-      return <CheckoutCarViewInfomation />;
-    case "#payment":
-      return 2;
-    default:
-      return "Not found";
-  }
+  const renderForm = () => {
+    switch (activeNavItem) {
+      case "#car":
+        return <CheckoutCarViewInfomation />;
+      case "#payment":
+        return <CheckoutPayment />;
+      case "#finance-option":
+        return <CheckoutFinanceOptions />;
+      default:
+        return "Not found";
+    }
+  };
+
+  return <FormContext {...methods}>{renderForm()}</FormContext>;
 }
