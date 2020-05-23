41fe37b9746fc0f173b22f8acb7185adc58aaff3
Ram4GB <leminhcuong2988@yahoo.com.vn>
2020-05-23 22:26:35 +0700

    fix

--- src/commons/hocs/Root.jsx
+++ src/commons/hocs/Root.jsx
@@ -6,7 +6,7 @@ import { loadStripe } from "@stripe/stripe-js";
 import MainPage from "./MainPage";
 import store from "../../modules/index";
 
-const stripePromise = loadStripe("pk_test_LSEzFDwGJ9o90CetcCkEoNw600qUPY2sU0"); // use public api key
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE); // use public api key
 
 export default function Root() {
   return (
--- src/pages/CheckoutVersionTwo.jsx
+++ src/pages/CheckoutVersionTwo.jsx
@@ -1,7 +1,6 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from "react";
 import { useSelector } from "react-redux";
-import { FormContext, useForm } from "react-hook-form";
 import { MODULE_NAME as MODULE_UI } from "../modules/ui/models";
 import CheckoutCarViewInfomation from "../modules/ui/components/CheckoutCarViewInfomation";
 import CheckoutPayment from "../modules/ui/components/CheckoutPayment";
@@ -9,20 +8,19 @@ import CheckoutFinanceOptions from "../modules/ui/components/CheckoutFinanceOpti
 
 export default function CheckoutVersionTwo() {
   const activeNavItem = useSelector(state => state[MODULE_UI].checkoutPage.activeNavItem);
-  const methods = useForm();
 
   const renderForm = () => {
     switch (activeNavItem) {
       case "#car":
         return <CheckoutCarViewInfomation />;
-      case "#payment":
-        return <CheckoutPayment />;
       case "#finance-option":
         return <CheckoutFinanceOptions />;
+      case "#payment":
+        return <CheckoutPayment />;
       default:
         return "Not found";
     }
   };
 
-  return <FormContext {...methods}>{renderForm()}</FormContext>;
+  return renderForm();
 }
