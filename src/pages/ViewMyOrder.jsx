b6b67b80eed82346dd8fe367ca57ebcd0d43908b
Hoang Tran <99.hoangtran@gmail.com>
2020-05-24 23:23:04 +0700

    saga oke

--- src/pages/ViewMyOrder.jsx
+++ src/pages/ViewMyOrder.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useRouteMatch, Redirect, useHistory } from "react-router-dom";
+import { MODULE_NAME as MODULE_USER } from "../modules/user/models";
+import * as actionsSagaOrder from "../modules/user/actionsSaga";
+
 import LayoutContentUser from "../commons/components/LayoutContentUser";
 
 export default function ViewMyOrder() {
+  const routeMatch = useRouteMatch();
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  const listOrders = useSelector(state => state[MODULE_USER].listOrders);
+  const currentOrder = useSelector(state => state[MODULE_USER].currentOrder);
+
+  useEffect(() => {
+    dispatch(actionsSagaOrder.fetchListOrders());
+    // dispatch(actionsSagaOrder.fetchOrder("YrvhV2GvD"));
+  }, []);
+
   return (
     <LayoutContentUser>
       <div>ViewMyOrder</div>
