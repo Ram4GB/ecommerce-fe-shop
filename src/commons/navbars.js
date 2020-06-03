20530b4f454b76f66ce5c761b2e39881d4cf1fe3
Hoang Tran <99.hoangtran@gmail.com>
2020-06-03 23:47:03 +0700

    better navbar

--- src/commons/navbars.js
+++ src/commons/navbars.js
@@ -1,16 +1,19 @@
 export const navbars = [
   {
-    name: "home",
+    name_en: "home",
+    name: "trang chủ",
     path: "/",
     subchild: []
   },
   {
-    name: "shop",
+    name_en: "shop",
+    name: "sản phẩm",
     path: "/search",
     subchild: []
   },
   {
-    name: "contact",
+    name_en: "contact",
+    name: "liên hệ",
     path: "/contact",
     subchild: []
   },
